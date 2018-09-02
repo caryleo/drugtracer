01493481948b84146fda9f702b9d79f3499536d2
zbraphael <zhangbo19970312@gmail.com>
2018-09-02 22:16:13 +0800

    测试测试

---
+++ contracts/hello.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.4.2;
+
+contract hello{
+
+            string re = "hello world";
+            function fa() public constant returns(string)
+            {
+                return re;
+            }
+
+         }
