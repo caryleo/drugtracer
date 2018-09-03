08e1653443b13873b56b079bbe227e15b5d8d548
caryleo <carysam@outlook.com>
2018-09-03 22:05:49 +0800

    修正了一些错误

--- contracts/producer.sol
+++ contracts/producer.sol
@@ -1,38 +1,29 @@
 pragma solidity ^0.4.0;
-//药厂��
+//药厂类
 contract Producer {
 
     //出厂信息
     struct ProduceDetail {
         string  drug;           //药品名称
         bool    state;          //合格状态
-        address prodecerCode;   //厂商地址
-        string  producer;       //厂商名称
-        string  producerDate;   //出厂日期
+        address producerCode;   //厂商地址
+        string  produceDate;   //出厂日期
     }
 
     //出厂信息映射
-    mapping (uint => ProduceDetail) produceList;
+    mapping (string => ProduceDetail) produceList;
 
     //流入市场信息
     struct InflowDetail {
-        uint    drugCode;   //药品批号
+        string  drugCode;   //药品批号
         uint    volume;     //交易量
-        address to;         //销售商地址
+        address toMerchant;         //销售商地址
     }
 
     //流入市场信息映射
     mapping (string => InflowDetail) inflowList;
 
     //流入记录
-    mapping (uint => string) simpleInflowList;
-
-
-
-
-
-    }
-
-
+    mapping (address => string[]) simpleInflowList;
 
 }
