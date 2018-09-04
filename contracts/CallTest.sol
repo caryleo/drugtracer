f9298ec6b14763b64067b422dcb8a7ae99660d6b
caryleo <carysam@outlook.com>
2018-09-04 15:39:58 +0800

    22

--- contracts/CallTest.sol
+++ contracts/CallTest.sol
@@ -3,8 +3,6 @@ pragma experimental ABIEncoderV2;
 contract CallTest{
     string[] b ;
     function array123()  constant public returns(string[]){
-
-
         b.push("123123");
         b.push("456");
         return b;
--- contracts/testadd.sol
+++ contracts/testadd.sol
@@ -1,12 +1,9 @@
 pragma solidity ^0.4.0;
 
 contract add{
-
     function addnumber(uint a, uint b) pure public returns(uint,uint){
         uint number1= a+b;
         uint number2= a*b;
-
-
         return (number1,number2);
     }
 
