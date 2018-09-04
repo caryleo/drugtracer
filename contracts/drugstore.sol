e8c21c5f65e32d77747d30c4227c68bf8f9d4207
zbraphael <zhangbo19970312@gmail.com>
2018-09-04 21:01:00 +0800

    修改

--- contracts/drugstore.sol
+++ contracts/drugstore.sol
@@ -7,7 +7,7 @@ contract Drugstore {
         string  circulateNumber;    //流出交易单号
         address customerNumber;     //消费者编号
         uint    volume;             //交易量
-        string  saleDate;           //销售日期  
+        string  saleDate;           //销售日期
     }
 
     //销售信息映射
