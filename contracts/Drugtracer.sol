269db5b80ce9e6039bfe0dcbf6451c4bec98837e
caryleo <carysam@outlook.com>
2018-09-05 19:33:04 +0800

    修改数据结构

--- contracts/Drugtracer.sol
+++ contracts/Drugtracer.sol
@@ -2,14 +2,16 @@ pragma solidity ^0.4.0;
 
 contract DrugTracer {
 
-    //------------------------------------------
+    //------药厂方数据结构-------------------
 
     //出厂信息
     struct ProduceDetail {
         string  drug;           //药品名称
         bool    state;          //合格状态
         address producerCode;   //厂商地址
-        string  produceDate;   //出厂日期
+        string  produceDate;    //出厂日期
+        uint    left;           //剩余量
+        bool    isValid;
     }
 
     //出厂信息映射
@@ -19,7 +21,9 @@ contract DrugTracer {
     struct InflowDetail {
         string  drugCode;   //药品批号
         uint    volume;     //交易量
-        address toMerchant;         //销售商地址
+        address toMerchant; //销售商地址
+        uint    left;       //剩余量
+        bool    isValid;
     }
 
     //流入市场信息映射
@@ -28,7 +32,7 @@ contract DrugTracer {
     //流入记录
     mapping (address => string[]) simpleInflowList;
 
-    //-------------------------------------------
+    //------销售商方数据结构----------------------
 
     //流通信息
     struct RollDetail {
@@ -36,6 +40,8 @@ contract DrugTracer {
         string  circulateDate;  //流通日期
         uint    volume;         //流通量
         address toDrugstore;    //药店编号
+        uint    left;           //剩余量
+        bool    isValid;
     }
 
     //流通信息映射
@@ -44,7 +50,7 @@ contract DrugTracer {
     //简化流出记录
     mapping (address=>string[]) simpleRollList;
 
-    //---------------------------------------------
+    //------药店方数据结构--------------------
 
     //销售信息
     struct SaleDetail {
@@ -52,6 +58,7 @@ contract DrugTracer {
         address customerNumber;     //消费者编号
         uint    volume;             //交易量
         string  saleDate;           //销售日期
+        bool    isValid;
     }
 
     //销售信息映射
@@ -60,7 +67,7 @@ contract DrugTracer {
     //简化销售记录
     mapping (address=>string[]) simpleSaleList;
 
-    //---------------------------------------------
+    //------消费者方数据结果---------------------
 
     //举报信息
     struct ReportDetail {
@@ -68,6 +75,7 @@ contract DrugTracer {
         string  reportDate; //举报日期
         string  report;     //举报内容
         bool    state;      //受理状态
+        bool    isValid;
     }
 
     //举报信息映射
@@ -76,12 +84,13 @@ contract DrugTracer {
     //简单举报信息映射
     mapping (address=>string[]) simplereportList;
 
-    //---------------------------------------------
+    //------监管部门方数据结构--------------------
 
     //受理信息
     struct DealDetail {
         string  dealDate;   //受理日期
         bool    result;     //受理结果
+        bool    isValid;
     }
 
     //受理信息映射
@@ -90,7 +99,7 @@ contract DrugTracer {
     //举报记录
     string[] reports;
 
-    //---------------------------------------------
+    //------药厂方set方法--------------------
 
     //插入出厂记录，state默认true，药品批号由前端生成，返回处理结果（布尔值）
     //drugCode:药品批号
@@ -102,11 +111,17 @@ contract DrugTracer {
                         string drugCode,
                         string drug,
                         address producer,
-                        string produceDate)
+                        string produceDate,
+                        uint left)
                         public returns (bool) {
-        ProduceDetail memory drugInfo = ProduceDetail(drug, true, producer, produceDate);
-        produceList[drugCode] = drugInfo;
-        return true;
+        ProduceDetail memory drugInfo = ProduceDetail(drug, true, producer, produceDate, left, true);
+        if (produceList[drugCode].isValid == false) {
+            produceList[drugCode] = drugInfo;
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     //插入流入市场记录，交易单号为交易的hash值，返回处理结果（布尔值）
@@ -121,17 +136,71 @@ contract DrugTracer {
                         uint volume,
                         address toMerchant)
                         public returns (bool) {
-        InflowDetail memory inflowInfo = InflowDetail(drugCode, volume, toMerchant);
-        inflowList[number] = inflowInfo;
-        setSimpleInflow(toMerchant, number);
-        return true;
+        InflowDetail memory inflowInfo = InflowDetail(drugCode, volume, toMerchant, volume, true);//初始情况下，剩余量就是交易量
+        if (inflowList[number].isValid == false) {
+            inflowList[number] = inflowInfo;
+            setSimpleInflow(toMerchant, number);
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     function setSimpleInflow (address to, string number) internal {
         simpleInflowList[to].push(number);
     }
 
-    //------------------------------------------------
+    //------销售商方set方法---------------------
 
-    
+    function setRoll (
+                        string number,
+                        string inflowNumber,
+                        string circulateDate,
+                        uint volume,
+                        address toDrugStore)
+                        public returns (bool) {
+        //TODO: R
+        return true;
+    }
+
+    //------药店方set方法-----------------------
+
+
+
+    //------消费者方set方法------------------------
+
+
+
+    //------监管部门方set方法----------------------------
+
+    function setReport (
+                        string number,
+                        string saleNumber,
+                        string reportDate,
+                        string report,
+                        bool state)
+                        public returns (bool) {
+        ReportDetail memory reportInfo = ReportDetail(saleNumber, reportDate, report, false, true);
+        reportList[number] = reportInfo;
+        return true;
+    }
+
+    //------药厂方get方法
+
+
+
+    //------销售商方get方法
+
+
+
+    //------药店方get方法
+
+
+
+    //------消费者方get方法
+
+
+
+    //------监管部门方get方法
 }
