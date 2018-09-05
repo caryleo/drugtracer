6bac85e13709c014e91c95d2e8fc20b29864d3b0
zbraphael <zhangbo19970312@gmail.com>
2018-09-05 14:51:06 +0800

    合约

--- contracts/CallTest.sol
+++ contracts/CallTest.sol
@@ -1,12 +1,16 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.20;
 pragma experimental ABIEncoderV2;
 contract CallTest{
-    bytes4 [] b ;
-    function array() public returns(bytes4 []){
-        /*b.push("12345678");
-        b.push("12345678");
-        b.push("12345678");
-        b.push("123456");*/
+    string[10] b ;
+    function array()  constant public returns(string[10]){
+       // b.push("12345678");
+       // b.push("0x123");
+       // b.push("12345678");
+       // b.push("123456");
+        b[0] = "123";
+        b[1] = "23333";
+        b[3] = "4444";
         return b;
     }
+
 }
