6cdcf396f56305016cd2ceb7099f55a3fa03549e
caryleo <carysam@outlook.com>
2018-09-07 19:49:37 +0800

    sss

--- contracts/hello.sol
+++ contracts/hello.sol
@@ -2,9 +2,14 @@ pragma solidity ^0.4.2;
 
 contract hello{
 
-    string re = "hello world";
-    function fa() public constant returns(string)
-    {
-        return re;
+    string ret = "Hello! MUYULING!";
+
+    function testInt(uint input) public pure returns (uint) {
+        uint ans = (input + 2) * 15 - 1;
+        return ans;
+    }
+
+    function testString(string input) public view returns (string, string) {
+        return (ret, input);
     }
 }
